7686d3d87b9d4d4778bf0ae2682d602db018d144
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2022-11-26 04:21:26 +0300

    fix: 22 tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -107,8 +107,10 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const s1 = Math.sqrt((x1 ** 2) + (y1 ** 2));
+  const s2 = Math.sqrt((x2 ** 2) + (y2 ** 2));
+  return Math.acos((x1 * x2) + (y1 * y2)) / (s1 * s2);
 }
 
 /**
