c49edc8fbb81e9f8e0b5ace2f3d70d03bb4ddabe
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2022-11-27 23:29:18 +0300

    fix: 52 tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -411,8 +411,25 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country < b.country) {
+      return -1;
+    }
+    if (a.country > b.country) {
+      return 1;
+    }
+    if (a.country === b.country) {
+      if (a.city < b.city) {
+        return -1;
+      }
+      if (a.city > b.city) {
+        return 1;
+      }
+      return 0;
+    }
+    return 0;
+  });
 }
 
 /**
