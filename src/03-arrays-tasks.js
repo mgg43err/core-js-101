ad4024a05fbcc60cac2afe25ac316b0b9ff9a886
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2022-11-27 22:06:13 +0300

    fix: 49 tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -347,8 +347,8 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.map((x) => Boolean(x)).filter((x) => x === false).length;
 }
 
 /**
