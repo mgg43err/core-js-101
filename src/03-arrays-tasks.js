5f58664095465666ea13e4177288e7ba7d52cb5b
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2022-11-27 21:46:37 +0300

    fix: 46 tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -295,8 +295,8 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((x) => x > 0 && typeof x === 'number').length;
 }
 
 /**
