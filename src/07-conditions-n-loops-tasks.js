68489aa6e7b179fdfd66d343e153abce6f8c24fb
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2023-03-06 16:14:37 +0300

    fix: 78 tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -76,8 +76,14 @@ function getFactorial(n) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  const a = [n1];
+  let p = n1;
+  while (p < n2) {
+    p += 1;
+    a.push(p);
+  }
+  return a.reduce((c, n) => c + n);
 }
 
 
